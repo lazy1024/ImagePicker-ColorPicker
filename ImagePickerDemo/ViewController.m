5c47a7fdec0f45e7263712f2dbab90943c88ac8b
lazy <lazy10240@126.com>
2015-06-29 15:05:39 +0800

    Initial Commit

---
+++ ImagePickerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImagePickerDemo
+//
+//  Created by magnimage on 15/6/29.
+//  Copyright (c) 2015年 Lazy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ImagePickerDemoTests/ImagePickerDemoTests.m
@@ -0,0 +1,40 @@
+//
+//  ImagePickerDemoTests.m
+//  ImagePickerDemoTests
+//
+//  Created by magnimage on 15/6/29.
+//  Copyright (c) 2015年 Lazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ImagePickerDemoTests : XCTestCase
+
+@end
+
+@implementation ImagePickerDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
